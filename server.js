04e7736eda282ca5ae2dab8351c39158eb8b300b
juargo <jorgeretamalaburto@gmail.com>
2017-01-07 20:43:55 -0300

    grafico diario

--- server.js
+++ server.js
@@ -5,4 +5,23 @@ app.use(express.static('public'));
 
 app.listen(3000, function () {
     console.log('Escuchando en 3000')
+});
+
+app.get('/getdataGraph',function (req,res) {  
+    var formula = req.query.formula;
+    formula = formula.replace(/ /g, '%20').replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/,/g, '%2C').replace(/\"/g, '%22');
+
+    var from = "00:00_20161205" ;
+    var until = "00:00_20161206";
+    var tz = "America/Santiago";
+    var format = "json";
+
+    var exec = require('child_process').execFile('public/bash.sh',
+        [
+            formula, from, until, tz, format
+        ]);
+
+    exec.stdout.on('data', function (data) {
+        res.json(data)
+    });
 })
