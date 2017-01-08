5bff3739120fa13ebdaf6560aac833908e8944fd
juargo <jorgeretamalaburto@gmail.com>
2017-01-07 21:15:42 -0300

    muestra formulas corregido

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ app.listen(3000, function () {
 app.get('/getmoiratriggers',function (req,res) {  
     var exec = require('child_process').execFile('public/bashtrigger.sh',[]);
     exec.stdout.on('data',function (data) { 
-        options="";
+        var options="";
         data = JSON.parse(data);
         for (nombre in data){
-            options = options + "<option value='"+ nombre +"' >"+ nombre + "</option>";
+            var idnombre = data[nombre].toString();
+            idnombre = idnombre.replace(/'/g, '\"').replace(/"/g, '\"');
+            options = options + "<option value='"+ idnombre +"' >"+ nombre + "</option>";
         }
         var select = '<select class="form-control input-sm" data-ng-model="selected" > \
                     ' + options + '\
