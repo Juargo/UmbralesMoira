cb830c372b805522b6a326d2f7ade44dbac7f1eb
juargo <jorgeretamalaburto@gmail.com>
2017-01-14 07:33:11 -0300

    dias funcinal con gráfico

--- public/controllers/graphFomulasController.js
+++ public/controllers/graphFomulasController.js
@@ -22,4 +22,74 @@ angular.module("graphApp")
             { fi: base + 6, ff: base + 7, class: "active", nombre: "Domingo" },
         ]
     }
+
+    $scope.tabactive = function (indi) {  
+         for(i=0;i<$scope.dias.length;i++){
+                    $scope.dias[i].clas="";
+                }
+                $scope.dias[indi].clas ="active";
+
+                //plot1.destroy();
+                //$scope.getUrl($scope.dias[indi].fi,$scope.dias[indi].ff)
+                //console.log($scope.urlg);
+                //console.log($scope.dias[indi].fi);
+                //console.log($scope.dias[indi].ff);
+
+                //plot1.destroy();
+                $scope.getUrl($scope.urlg,$scope.dias[indi].fi,$scope.dias[indi].ff)
+
+    }
+
+    $scope.getUrl = function(urlg,fi,ff){
+            var puntosn=[];
+                var pw1=[];
+                var pw2=[];
+                //var urlg = "aliasByNode(summarize(gwpromo.compra.general.estado.Ok, \"10min\", \"sum\"), 4)"
+                //aliasByNode(summarize(gwpromo.compra.general.estado.Ok, "10min", "sum"), 4)
+                
+                $.get("http://localhost:3000/getdataGraph?formula=" + urlg + "&fi=" + fi + "&ff=" + ff,function( data ) {
+                    data = JSON.parse(data);
+                    puntos = data[0].datapoints;
+                    cantidad = puntos.length;
+                    for (var i = 0; i < puntos.length; i++) {
+                        var t = new Date(puntos[i][1] * 1000);
+                        var tt = t.toGMTString();
+                        var tt = tt.substr(0,25);
+                        ptos_c =puntos[i][0]+0
+                        puntosn.push([tt,ptos_c]);
+                  
+                        if(ptos_c < 10){
+                            pw2.push([tt,0]);
+                        }else{
+                            pw2.push([tt,ptos_c*1.2]);
+                        }
+                   
+                        pw1.push([tt,puntos[i][0]*0.8]);
+                    }
+                    $.jqplot.config.enablePlugins = true;
+                    data = [puntosn,pw1,pw2] ;
+
+                    plot1 = $.jqplot('myChart',data,{
+                        title: "Umbrales",
+                        axes:{
+                            xaxis:{
+                                renderer: $.jqplot.DateAxisRenderer,
+                                tickRenderer: $.jqplot.CanvasAxisTickRenderer,
+                                tickOptions:{
+                                    formatString: "%H:%M",
+                                    angle:-45
+                                }
+                            }
+                        },
+                        highligther:{
+                            sizeAdjust:10,
+                            tooltipLocation:'n',
+                            tooltipAxes:'both',
+                            tooltipFormatString:'<b><i><span>%s , </span></i></b> %.2f',
+                            useAxesFormatters:false
+                        }
+                    })
+                })
+                }
+
 })
--- server.js
+++ server.js
@@ -11,8 +11,20 @@ app.get('/getdataGraph',function (req,res) {
     var formula = req.query.formula;
     formula = formula.replace(/ /g, '%20').replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/,/g, '%2C').replace(/\"/g, '%22');
 
-    var from = "00:00_20161205" ;
-    var until = "00:00_20161206";
+    var fi = req.query.fi;
+    var ff = req.query.ff;
+    //var from = "00:00_20161205" ;
+    //var until = "00:00_20161206";
+
+    if(fi<10){
+        fi = "0"+fi;
+    }
+    if(ff<10){
+        ff = "0"+ff;
+    }
+
+    var from = "00:00_201612" + fi ;
+    var until = "00:00_201612" + ff;
     var tz = "America/Santiago";
     var format = "json";
 
