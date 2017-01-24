7acb22a3b92ee42cae48dfd67b92d0b3840e4e57
juargo <jorgeretamalaburto@gmail.com>
2017-01-24 19:40:55 -0300

    se verifica si tiene umbrales dicho trigger

--- public/controllers/moiraController.js
+++ public/controllers/moiraController.js
@@ -1,5 +1,6 @@
 angular.module("graphApp")
-        .controller("moiraCtrl",function ($scope,$compile) {  
+        .controller("moiraCtrl",function ($scope,$compile,$http) {  
+            $scope.triggerguradado = true;
             $.ajax({
                 url:"http://localhost:3000/getmoiratriggers",
                 type: "GET",
@@ -7,4 +8,18 @@ angular.module("graphApp")
                     $("#moiratriggerselect").append($compile(data)($scope));
                 }
             });
+
+            $scope.triggerSelect = function () {  
+                var trigger =$( "#select option:selected" ).text();
+                $http.get("http://localhost:3000/getbyname/" + trigger).then(
+                    function (resp) {
+                        if(parseInt(resp.data.length,10) == 0){
+                            $scope.triggerguradado = false;
+                        }else{
+                            $scope.triggerguradado = true;
+                        }
+                    }
+                )
+            }
+            console.log($scope.triggerguradado)
         })
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
@@ -17,9 +25,40 @@ app.get('/getmoiratriggers',function (req,res) {
             idnombre = idnombre.replace(/'/g, '\"').replace(/"/g, '\"');
             options = options + "<option value='"+ idnombre +"' >"+ nombre + "</option>";
         }
-        var select = '<select class="form-control input-sm" data-ng-model="selected" > \
+        var select = '<select class="form-control input-sm" data-ng-model="triggerSelected" ng-change="triggerSelect()" id="select"> \
                     ' + options + '\
                     </select>';
         res.send(select);
      })
 })
+
+mongoose.connect('mongodb://localhost:27017/UmbralesMoira');
+
+umbrales = mongoose.model('Umbral', {
+    trigger: String,
+    formulas: [
+        {
+            nombre: String,
+            formula: String,
+            datapoint: []
+        }
+    ]
+});
+
+app.get('/getid/:id', function (req, res) {
+    umbrales.findById(req.params.id, function (err, data) {
+        if (err)
+            res.send(err);
+        res.json(data);
+    })
+})
+
+app.get('/getbyname/:nombre', function (req, res) {
+    umbrales.find({ 'trigger': req.params.nombre }, function (err, data) {
+        if (err)
+            res.send(err);
+        res.json(data);
+    })
+})
+
+
