b04c87d63e4b4d568784e235239fc2e6666f7d96
juargo <jorgeretamalaburto@gmail.com>
2017-02-09 19:22:03 -0300

    se corrige entra de fechas de la semana

--- public/controllers/baseController.js
+++ public/controllers/baseController.js
@@ -1,5 +1,5 @@
 angular.module("umbralesApp")
-    .controller("baseController", function ($scope, $http, trigger,plot) {
+    .controller("baseController", function ($scope, $http, trigger, plot) {
         $scope.triggerguardado = true;
         $scope.grafica = false;
 
@@ -33,17 +33,64 @@ angular.module("umbralesApp")
             trigger.setntrigger(triggery);
         }
 
-        var beforeOneWeek = new Date(new Date().getTime() - 60 * 60 * 24 * 7 * 1000)
-        day = beforeOneWeek.getDay()
-        diffToMonday = beforeOneWeek.getDate() - day + (day === 0 ? -6 : 1)
-        $scope.lastLunes = new Date(beforeOneWeek.setDate(diffToMonday))
-        $scope.lastMartes = new Date(beforeOneWeek.setDate(diffToMonday + 1))
-        $scope.lastMiercoles = new Date(beforeOneWeek.setDate(diffToMonday + 2))
-        $scope.lastJueves = new Date(beforeOneWeek.setDate(diffToMonday + 3))
-        $scope.lastViernes = new Date(beforeOneWeek.setDate(diffToMonday + 4))
-        $scope.lastSabado = new Date(beforeOneWeek.setDate(diffToMonday + 5))
-        $scope.lastDomingo = new Date(beforeOneWeek.setDate(diffToMonday + 6));
+        // var beforeOneWeek = new Date(new Date().getTime() - 60 * 60 * 24 * 7 * 1000)
+        // day = beforeOneWeek.toString().split(/ /g)[2];
+        // diffToMonday = beforeOneWeek.getDate() - day + (day === 0 ? -6 : 1);
+        // $scope.lastLunes = new Date(beforeOneWeek.setDate(diffToMonday))
+        // $scope.lastMartes = new Date(beforeOneWeek.setDate(diffToMonday + 1))
+        // $scope.lastMiercoles = new Date(beforeOneWeek.setDate(diffToMonday + 2))
+        // $scope.lastJueves = new Date(beforeOneWeek.setDate(diffToMonday + 3))
+        // $scope.lastViernes = new Date(beforeOneWeek.setDate(diffToMonday + 4))
+        // $scope.lastSabado = new Date(beforeOneWeek.setDate(diffToMonday + 5))
+        // $scope.lastDomingo = new Date(beforeOneWeek.setDate(diffToMonday + 6));
 
+        //Mon Lunes
+        //Tue Martes
+        //Wed Miercoles
+        //Thu Jueves
+        //Fri Viernes
+        //Sat Sabado
+        //Sun Domingo
+
+        var mes=[];
+        mes['Jan'] = '01';
+        mes['Feb'] = '02';
+        mes['Mar'] = '03';
+        mes['Apr'] = '04';
+        mes['May'] = '05';
+        mes['Jun'] = '06';
+        mes['Jul'] = '07';
+        mes['Aug'] = '08';
+        mes['Sep'] = '09';
+        mes['Oct'] = '10';
+        mes['Nov'] = '11';
+        mes['Dec'] = '12';
+
+        date = new Date();
+        date = date.toString().split(/ /g);
+
+        var dia=[];
+        dia['Mon'] = 7;
+        dia['Tue'] = 8;
+        dia['Wed'] = 9;
+        dia['Thu'] = 10;
+        dia['Fri'] = 11;
+        dia['Sat'] = 12;
+        dia['Sun'] = 13;
+
+        dd =  new Date(mes[date[1]]+ '-' +  date[2]+ '-' + date[3]);
+        dd.setDate(dd.getDate() - dia[date[0]]);
+        
+        base=dd;
+        $scope.lastLunes = new Date(base);
+        $scope.lastMartes = new Date(base.setDate(dd.getDate() + 1));
+        $scope.lastMiercoles = new Date(base.setDate(dd.getDate() + 1));
+        $scope.lastJueves = new Date(base.setDate(dd.getDate() + 1));
+        $scope.lastViernes = new Date(base.setDate(dd.getDate() + 1));
+        $scope.lastSabado = new Date(base.setDate(dd.getDate() + 1));
+        $scope.lastDomingo = new Date(base.setDate(dd.getDate() + 1));
+
+        
         //00:00_20161205
         $scope.baseSelected = function () {
             $scope.dias = [
@@ -59,7 +106,7 @@ angular.module("umbralesApp")
 
 
         $scope.tabactive = function (indi) {
-            formula1 = $scope.triggers[trigger.getntrigger()].replace(/ /g,"");
+            formula1 = $scope.triggers[trigger.getntrigger()].replace(/ /g, "");
             getUrl(formula1, $scope.dias[indi].fi)
             //console.log($scope.triggers[trigger.getntrigger()].replace(/ /g,""));
         }
